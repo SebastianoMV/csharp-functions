770b14d78387eeb471b8fb8316e0fa5dafe15be2
SebastianoMV <sebastianovisco@gmail.com>
2022-09-17 14:23:54 +0200

    Preparazione funzioni

--- Program.cs
+++ Program.cs
@@ -1,2 +1,48 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int[] arrayNumeri = { 2, 6, 7, 5, 3, 9 };
+
+
+void StampaArray(int[] array)
+{
+    Console.Write('[');
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(" " + array[i]);
+        if (i < (array.Length -1 )) Console.Write('\u002C');
+    }
+    Console.WriteLine(']');
+};
+
+
+int Quadrato(int numero)
+{
+    return (numero * numero);
+};
+
+
+int[] ElevaArrayAlQuadrato(int[] array)
+{
+    int[] nuvoArray;
+    nuvoArray = (int[])array.Clone();
+
+    for (int i = 0; i < nuvoArray.Length; i++)
+    {
+        nuvoArray[i] = Quadrato(nuvoArray[i]);
+    }
+    return nuvoArray;
+}
+
+
+int sommaElementiArray(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        sum = sum + array[i];
+    }
+    return sum; 
+
+};
+
+StampaArray(arrayNumeri);
